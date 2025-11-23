14eeaa9cef4799987aa58c5148a36257d9dd6a9b
llarchev <lucaslarchevque@gmail.com>
2025-11-23 21:25:08 +0100

    fix: default sort order now A-Z instead of Z-A

--- assets/js/store.js
+++ assets/js/store.js
@@ -3,6 +3,6 @@ export const store = {
 	filteredHeroes: [],
 	pageSize: 20,
 	currentPage: 1,
-	currentSortColumn: "", // Mettre une chaîne vide au lieu de "name"
+	currentSortColumn: "name",
 	currentSortOrder: "asc"
 };
